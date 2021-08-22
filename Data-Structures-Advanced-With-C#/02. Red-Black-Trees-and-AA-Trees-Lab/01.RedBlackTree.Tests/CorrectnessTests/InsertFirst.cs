f6041a7393fb50322635ad0eaa2a150b291ecbbd
dimitrovmartin <esdiedno@gmail.com>
2021-08-22 15:22:58 +0300

    Added Red-Black-Tree Insert

---
+++ Data-Structures-Advanced-With-C#/02. Red-Black-Trees-and-AA-Trees-Lab/01.RedBlackTree.Tests/CorrectnessTests/InsertFirst.cs
@@ -0,0 +1,15 @@
+﻿using _01.RedBlackTree;
+using NUnit.Framework;
+
+[TestFixture]
+class InsertFirst
+{
+    [Test]
+    public void Insert_SingleElement_ShouldIncreaseCount()
+    {
+        RedBlackTree<int> rbt = new RedBlackTree<int>();
+        rbt.Insert(5);
+
+        Assert.AreEqual(1, rbt.Count());
+    }
+}
---
+++ Data-Structures-Advanced-With-C#/02. Red-Black-Trees-and-AA-Trees-Lab/01.RedBlackTree.Tests/TimeTests/InsertFourth.cs
@@ -0,0 +1,19 @@
+﻿using _01.RedBlackTree;
+using NUnit.Framework;
+
+[TestFixture]
+class InsertFourth
+{
+    [Test]
+    [Timeout(500)]
+    public void Insert_MultipleElements_ShouldBeFast()
+    {
+        RedBlackTree<int> rbt = new RedBlackTree<int>();
+
+        for (int i = 0; i < 100000; i++)
+        {
+            rbt.Insert(i);
+        }
+        
+    }
+}
