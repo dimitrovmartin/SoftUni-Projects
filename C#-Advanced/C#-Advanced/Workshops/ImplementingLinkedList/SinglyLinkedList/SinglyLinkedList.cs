98c5c72fea1dd3731248888e72b835e37c043dbd
dimitrovmartin <esdiedno@gmail.com>
2021-08-22 19:58:40 +0300

    Added Workshops from C# Advanced course

---
+++ C#-Advanced/C#-Advanced/Workshops/ImplementingLinkedList/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,133 @@
+﻿namespace SinglyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class SinglyLinkedList<T> : IEnumerable<T>
+    {
+        private Node<T> head;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            Node<T> newNode = new Node<T>(item);
+
+            if (Count == 0)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            Node<T> newNode = new Node<T>(item);
+
+            if (Count == 0)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node<T> currentNode = head;
+
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                currentNode.Next = newNode;
+            }
+
+            Count++;
+        }
+
+        public T GetFirst()
+        {
+            IsListEmpty();
+
+            return head.Value;
+        }
+
+        public T GetLast()
+        {
+            IsListEmpty();
+
+            Node<T> currentNode = head;
+
+            while (currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            return currentNode.Value;
+        }
+
+        public T RemoveFirst()
+        {
+            IsListEmpty();
+
+            Node<T> oldHead = head;
+
+            head = head.Next;
+
+            Count--;
+
+            return oldHead.Value;
+        }
+
+        public T RemoveLast()
+        {
+            IsListEmpty();
+
+            Node<T> currentNode = head;
+            Node<T> secondCurrentNode = head;
+
+            while (currentNode.Next != null)
+            {
+                secondCurrentNode = currentNode;
+                currentNode = currentNode.Next;
+            }
+
+            Node<T> nodeToReturn = currentNode;
+
+            secondCurrentNode.Next = null;
+            currentNode = null;
+
+            Count--;
+
+            return nodeToReturn.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> currentNode = head;
+
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+
+                currentNode = currentNode.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void IsListEmpty()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ C#-Advanced/C#-Advanced/Workshops/ImplementingStackAndQueue/List/List.cs
@@ -0,0 +1,162 @@
+﻿namespace List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class List<T> : IEnumerable<T>
+    {
+        private const int DEFAULT_CAPACITY = 4;
+        private T[] arr;
+
+         public List(int capacity = DEFAULT_CAPACITY)
+        {
+            arr = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                IsValidIndex(index);
+
+                return arr[index];
+            }
+            set
+            {
+                IsValidIndex(index);
+
+                arr[index] = value;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            ResizeIfNessecary();
+
+            arr[Count] = item;
+
+            Count++;
+        }
+        
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (arr[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (arr[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            IsValidIndex(index);
+            ResizeIfNessecary();
+            ShiftRight(index);
+
+            arr[index] = item;
+
+            Count++;
+        }
+        
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (arr[i].Equals(item))
+                {
+                    ShiftLeft(i);
+
+                    Count--;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        public void RemoveAt(int index)
+        {
+            IsValidIndex(index);
+
+            ShiftLeft(index);
+
+            Count--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return arr[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private void IsValidIndex(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        private void ResizeIfNessecary()
+        {
+            if (Count == arr.Length)
+            {
+                Resize();
+            }
+        }
+
+        private void Resize()
+        {
+            T[] arrCopy = new T[arr.Length * 2];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrCopy[i] = arr[i];
+            }
+
+            arr = arrCopy;
+        }
+        
+        private void ShiftRight(int index)
+        {
+            for (int i = Count; i >= index; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+        }
+    }
+}
