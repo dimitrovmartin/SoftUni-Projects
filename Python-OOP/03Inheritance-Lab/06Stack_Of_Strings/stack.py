c4f88a7fb9ac41bbed49a0ec976921ae64906b69
dimitrovmartin <esdiedno@gmail.com>
2021-09-13 14:36:15 +0300

    Added Inheritance-Lab

---
+++ Python-OOP/03Inheritance-Lab/06Stack_Of_Strings/stack.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, element):
+        self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def __str__(self):
+        return f'[{", ".join(self.data[::-1])}]'
